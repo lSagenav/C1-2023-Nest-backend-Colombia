df8fa8c8802b0c41d21f1418615053c92d7f60f8
JuanVanegas <44687061+lSagenav@users.noreply.github.com>
2023-02-02 16:39:38 -0500

    TITLULO: apatrones de diseño DESCRIPCION: se agrega el patron de diseño observe

---
+++ src/day3Week2/observer.ts
@@ -0,0 +1,39 @@
+interface Observer {
+  update(): void;
+}
+
+interface subject {
+  subscribe(observer: Observer): void;
+  unsubscribe(observer: Observer): void;
+  notify(): void;
+}
+
+class Observable implements subject {
+  private observers: Observer[] = [];
+
+  subscribe(observer: Observer): void {
+    this.observers.push(observer);
+  }
+  unsubscribe(observer: Observer): void {
+    this.observers = this.observers.filter((obs) => obs !== observer);
+  }
+  notify(): void {
+    this.observers.forEach((observer) => observer.update());
+  }
+}
+
+class SomeObserve implements Observer {
+  update(): void {
+    console.log('Evento');
+  }
+}
+
+const ob = new Observable();
+const o = new SomeObserve();
+const o2 = new SomeObserve();
+
+ob.subscribe(o);
+ob.subscribe(o2);
+ob.notify();
+ob.unsubscribe(o);
+ob.notify();
---
+++ src/day3Week2/strategy.ts
@@ -0,0 +1,8 @@
+class Transpiler {
+  public transpilate(): void {
+    console.log('Transpilation to java');
+  }
+}
+
+const transpiler = new Transpiler();
+transpiler.transpilate();
