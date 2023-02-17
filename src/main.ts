df2764f619d8e44ee1d024152ae72c67580d1840
lsagenav <jcvanegas@unac.edu.co>
2023-02-17 11:39:05 -0500

    TITULO: update transfer DESCRIPCION: se corrigen los permisos de los cors

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,24 @@
 // Libraries
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
-
-// Configurations
-import { PipeValidatorConfig } from './configs';
 
 // Main module
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://example.com',
+      'http://www.example.com',
+      'http://app.example.com',
+      'https://example.com',
+      'https://www.example.com',
+      'https://app.example.com',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 
